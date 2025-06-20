7d4c20e3caa3d31368ee4014a0ec9cfcbdd9ab83
Rahul Panchta <panchtarahul@outlook.com>
2025-06-20 20:27:14 +0530

    url

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL:import.meta.env.VITE_BACKEND_URL,
-  withCredentials: false,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true, // true if using refresh tokens or cookies
 });
 
 export default Axios;
